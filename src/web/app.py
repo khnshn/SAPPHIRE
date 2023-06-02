0054572a7cb7c79c6c3816def59976735b369eb4
khnshn <programming.khanshan@gmail.com>
2023-06-02 14:56:45 +0200

    enable file upload

--- src/web/app.py
+++ src/web/app.py
@@ -25,5 +25,6 @@ def simulate():
     elif request.args.get("type") == "custom":
         params = [(x, request.args.get(x)) for x in request.args]
         params.remove(params[0])  # to remove type=custom; irrelevant for the simulator
+        print(request.files)
         return jsonify(sim_web.simulate_custom_get_data(params)), 200
     return "Unknown simulation type", 400
