7b063abb642482e79d55c4160648638c8220dcf0
khnshn <programming.khanshan@gmail.com>
2023-06-05 12:03:07 +0200

    fix joblib dependency

--- src/web/app.py
+++ src/web/app.py
@@ -5,6 +5,7 @@ from flask import jsonify
 import simulate as sim_web
 import numpy as np
 from io import BytesIO
+import joblib
 
 app = Flask(__name__)
 
@@ -28,11 +29,16 @@ def simulate():
         params = [(x, request.args.get(x)) for x in request.args]
         params.remove(params[0])  # to remove type=custom; irrelevant for the simulator
         upload_data = {}
-        for filename in request.files:
-            contents = BytesIO(request.files[filename].read())
-            data = np.genfromtxt(contents, delimiter=",")
-            upload_data[filename] = data
-            print(upload_data)
+        for file in request.files:
+            if request.files[file].filename.endswith(".csv"):
+                contents = BytesIO(request.files[file].read())
+                data = np.genfromtxt(contents, delimiter=",")
+                upload_data[file] = data
+            elif request.files[file].filename.endswith(".joblib"):
+                contents = BytesIO(request.files[file].read())
+                clf = joblib.load(contents)
+            else:
+                return "Unknown simulation type", 400
             print(params)
         return jsonify(sim_web.simulate_custom_get_data(params)), 200
     return "Unknown simulation type", 400
